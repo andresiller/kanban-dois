801101c977968138424d93972d49fd3857fcb00e
Andre <andresiller.es@gmail.com>
2025-09-04 19:15:06 -0300

    first

---
+++ app/src/main/java/com/andre/task/ui/DoneFragment.kt
@@ -0,0 +1,82 @@
+package com.andre.task.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.andre.task.R
+import com.andre.task.data.model.Status
+import com.andre.task.data.model.Task
+import com.andre.task.databinding.FragmentDoneBinding
+import com.andre.task.ui.adapter.TaskAdapter
+
+class DoneFragment : Fragment() {
+
+    private var _binding: FragmentDoneBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var taskAdapter: TaskAdapter
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentDoneBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initListeners()
+        initRecyclerViewTask(getTask())
+    }
+
+    private fun initListeners(){
+        binding.floatingActionButton2.setOnClickListener {
+            findNavController().navigate(R.id.action_homeFragment_to_formTaskFragment)
+        }
+    }
+
+    private fun initRecyclerViewTask(taskList: List<Task>){
+        taskAdapter = TaskAdapter(requireContext(), taskList) {task, option -> optionSelected(task,option)}
+        binding.recyclerViewTask.layoutManager = LinearLayoutManager(requireContext())
+        binding.recyclerViewTask.setHasFixedSize(true)
+        binding.recyclerViewTask.adapter = taskAdapter
+    }
+
+    private fun optionSelected(task:Task, option: Int){
+        when (option){
+            TaskAdapter.SELECT_REMOVER -> {
+                Toast.makeText(requireContext(), "Removendo ${task.description}", Toast.LENGTH_SHORT).show()
+            }
+            TaskAdapter.SELECT_EDIT -> {
+                Toast.makeText(requireContext(), "Editando ${task.description}", Toast.LENGTH_SHORT).show()
+            }
+            TaskAdapter.SELECT_DETAILS -> {
+                Toast.makeText(requireContext(), "Detalhes ${task.description}", Toast.LENGTH_SHORT).show()
+            }
+            TaskAdapter.SELECT_BACK -> {
+                Toast.makeText(requireContext(), "Anterior", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun getTask() = listOf(
+        Task("0", "Implementar autenticação via impressão digital", Status.DONE),
+        Task("1", "Finalizar integração com API de clima", Status.DONE),
+        Task("2", "Ajustar layout responsivo da tela de configurações", Status.DONE),
+        Task("3", "Concluir testes automatizados do módulo de login", Status.DONE),
+        Task("4", "Publicar primeira versão beta na Play Store", Status.DONE),
+    )
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
